66091ac21ff89fb5bbaf4b58408ee7964185b260
gfeliciodias <gabrielfeliciods@gmail.com>
2026-03-10 16:47:25 -0300

    Alterações e adições no ex2

--- Pratica01EX02/src/main/java/com/mycompany/pratica01ex02/Pessoa.java
+++ Pratica01EX02/src/main/java/com/mycompany/pratica01ex02/Pessoa.java
@@ -9,84 +9,72 @@ package com.mycompany.pratica01ex02;
  * @author Usuario
  */
 public class Pessoa {
-
     /**
      * @return the Nome
      */
     public String[] getNome() {
         return Nome;
     }
-
     /**
      * @param Nome the Nome to set
      */
     public void setNome(String[] Nome) {
         this.Nome = Nome;
     }
-
     /**
      * @return the Sobrenome
      */
     public String[] getSobrenome() {
         return Sobrenome;
     }
-
     /**
      * @param Sobrenome the Sobrenome to set
      */
     public void setSobrenome(String[] Sobrenome) {
         this.Sobrenome = Sobrenome;
     }
-
     /**
      * @return the idade
      */
     public int getIdade() {
         return idade;
     }
-
     /**
      * @param idade the idade to set
      */
     public void setIdade(int idade) {
         this.idade = idade;
     }
-
     /**
      * @return the altura
      */
     public float getAltura() {
         return altura;
     }
-
     /**
      * @param altura the altura to set
      */
     public void setAltura(float altura) {
         this.altura = altura;
     }
-
     /**
      * @return the peso
      */
     public float getPeso() {
         return peso;
     }
-
     /**
      * @param peso the peso to set
      */
     public void setPeso(float peso) {
         this.peso = peso;
     }
-
     /**
      * @return the imc
      */
     public float getImc() {
         return imc;
     }
-
     /**
      * @param imc the imc to set
      */
