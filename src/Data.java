0ad9d35b0d1f435c6edcbeab36db58e8be27ff56
Gabriel Felicio Dias <gabrielfeliciods@gmail.com>
2026-03-16 12:16:22 -0300

    Add files via upload
    Começo

---
+++ src/Data.java
@@ -0,0 +1,64 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pratica01pt02;
+
+/**
+ *
+ * @author 733633
+ */
+public class Data {
+    private int dia;
+    private int mes;
+    private int ano;
+
+    /**
+     * @return the dia
+     */
+    public int getDia() {
+        return dia;
+    }
+
+    /**
+     * @param dia the dia to set
+     */
+    public void setDia(int dia) {
+        this.dia = dia;
+    }
+
+    /**
+     * @return the mes
+     */
+    public int getMes() {
+        return mes;
+    }
+
+    /**
+     * @param mes the mes to set
+     */
+    public void setMes(int mes) {
+        this.mes = mes;
+    }
+
+    /**
+     * @return the ano
+     */
+    public int getAno() {
+        return ano;
+    }
+
+    /**
+     * @param ano the ano to set
+     */
+    public void setAno(int ano) {
+        this.ano = ano;
+    }
+    
+    
+    
+    
+    
+    
+    
+}
